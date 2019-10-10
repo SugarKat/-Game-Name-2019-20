31702791b1ac8d2499df32263a25e543fabad92c
SugarKat <46972873+SugarKat@users.noreply.github.com>
2019-10-10 20:37:17 +0300

    dont know

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameManager : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
