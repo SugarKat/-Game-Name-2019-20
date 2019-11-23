df0cb1808127e07efcad2333455e8bad46758a26
LoveLess420 <56192734+LoveLess420@users.noreply.github.com>
2019-11-23 12:06:23 -0800

    Camera Following Script

---
+++ CameraFollowing.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraFollow : MonoBehaviour
+{
+
+
+    private Transform playerTransform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+    void LateUpdate()
+    {
+        // we store current camera´s position in variable temp - temporary position
+        Vector3 temp = transform.position;
+
+
+        // we set the cameras position x to be equal to the players position x
+        temp.x = playerTransform.position.x;
+        temp.y = playerTransform.position.y;
+
+
+        // we set back the cameras temo position to the cameras current position
+        transform.position = temp;
+    }
+}
